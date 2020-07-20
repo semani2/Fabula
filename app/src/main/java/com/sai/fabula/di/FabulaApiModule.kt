9a2f9e92ed8ba8448cca26b934856d9ffe815ced
Dennis Marcu <dennis.marcu@wishabi.com>
2020-07-20 12:06:05 -0400

    swapping koin for hilt

--- app/src/main/java/com/sai/fabula/di/FabulaApiModule.kt
+++ app/src/main/java/com/sai/fabula/di/FabulaApiModule.kt
@@ -3,11 +3,18 @@ package com.sai.fabula.di
 import com.sai.fabula.api.FabulaApiService
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
+@Module
+@InstallIn(ApplicationComponent::class)
 class FabulaApiModule {
 
+    @Provides
     fun getFabulaApiService(): FabulaApiService = Retrofit.Builder()
         .baseUrl(FabulaApiService.NEWS_API_URL)
         .addConverterFactory(MoshiConverterFactory.create(
